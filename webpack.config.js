5e78f25d234ff90f7b482688c9fe7f99196dbaea
Tao Ning <ningtaous@gmail.com>
2018-10-08 00:18:20 -0400

    Config webpack properly to import to Neptune
    1. Compile ES6 to ES5
    2. Turn off mangleWasmImports

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,26 @@ module.exports = {
     library: 'srtValidator',
     libraryTarget: 'amd',
   },
+  optimization: {
+    mangleWasmImports: false,
+  },
   resolve: {
     alias: {
       'srt-validator': path.resolve(__dirname, 'src'),
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
