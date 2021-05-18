a97a13bc737045cccc091921020a18cabeb960a3
Yicheng Gong <yigong@linkedin.com>
2021-05-17 17:28:06 -0700

    add support for es6

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: [['@babel/preset-env', { modules: 'umd' }]],
+            plugins: ['add-module-exports'],
           },
         },
       },
